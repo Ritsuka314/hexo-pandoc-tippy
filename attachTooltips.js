2f3eb2db1cd7f1ca5fe9f705f4b53b7dd6a9475b
Richard Yan <richardyan314@foxmail.com>
2019-05-06 16:52:58 +0800

    use DOM2 listener instead of window.load to avoid conflict

--- attachTooltips.js
+++ attachTooltips.js
@@ -1,8 +1,8 @@
-window.onload = function (ev) {
+window.addEventListener("load", function (ev) {
     let pattern = /^(fnref)(.+)$/;
     let base = {html: e1 => '#tooltip' + pattern.exec(e1.id)[2].replace(/\./g, "\\.")}
     let extra = %s
     
     tippy('a[id^="fnref"]',
           Object.assign({},base, extra))
-};
+});
