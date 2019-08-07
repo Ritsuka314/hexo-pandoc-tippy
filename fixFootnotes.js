73d875885ef9da600fac73f5af65ba459e6624fc
RichardYan <richardyan314@foxmail.com>
2019-08-07 15:41:42 +0800

    fix #4
    Thanks to @Core00077 and @claviska

--- fixFootnotes.js
+++ fixFootnotes.js
@@ -1,5 +1,36 @@
 module.exports = function (doc) {
   const cheerio = require('./node_modules/cheerio');
+    
+  // https://github.com/cheeriojs/cheerio/issues/866#issuecomment-482730997
+  const load = cheerio.load;
+
+  function decode(string) {
+    return string.replace(/&#x([0-9a-f]{1,6});/ig, (entity, code) => {
+      code = parseInt(code, 16);
+
+      // Don't unescape ASCII characters, assuming they're encoded for a good reason
+      if (code < 0x80) return entity;
+
+      return String.fromCodePoint(code);
+    });
+  }
+
+  function wrapHtml(fn) {
+    return function() {
+      const result = fn.apply(this, arguments);
+      return typeof result === 'string' ? decode(result) : result;
+    };
+  }
+
+  cheerio.load = function() {
+    const instance = load.apply(this, arguments);
+
+    instance.html = wrapHtml(instance.html);
+    instance.prototype.html = wrapHtml(instance.prototype.html);
+
+    return instance;
+  };
+  
   var $ = cheerio.load(doc.content);
   
   // cheerio seems to be wrapping input in <html><body></body></html>
